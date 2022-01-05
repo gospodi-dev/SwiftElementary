0e4d1ed90f31be4338bdec5b1cdc4e09b4797010
Gospodi <gospodijazzet@gmail.com>
2022-01-05 20:34:20 +0300

    Добавлен - Многострочный комментарий (multi-line comment)

--- 01. Expressions, Variables & Constants.playground/Contents.swift
+++ 01. Expressions, Variables & Constants.playground/Contents.swift
@@ -10,6 +10,24 @@ import UIKit
 // This is a comment. It is not executed. (Это комментарий. Он не выполняется.)
 
 // Несколько однострочных комментариев сложенных в абзац
-// This is also a comment.
-// Over multiple lines.
+// This is also a comment. (Это тоже комментарий)
+// Over multiple lines. (На несколько строк)
 
+// ВТОРОЙ СПОСОБ. Многострочный комментарий (multi-line comment)
+
+/* This is also a comment.
+   Over many..
+   many...
+   many lines. */
+
+// Пример многострочного комментария с вложенным комментарием
+/*
+ This is a comment.
+ 
+     /* And inside it
+      is
+      another comment.
+      */
+ 
+ Back to the first
+ */
