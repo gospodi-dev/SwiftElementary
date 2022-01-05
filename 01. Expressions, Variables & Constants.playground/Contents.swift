b85104d479ceb6566b2ef8b421a642c460fbf1c7
Gospodi <gospodijazzet@gmail.com>
2022-01-05 20:00:31 +0300

    Рассмотрим тему добавления комментариев. Первый способ - Однострочный комментарий (single-line comment)

--- 01. Expressions, Variables & Constants.playground/Contents.swift
+++ 01. Expressions, Variables & Constants.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//var greeting = "Hello, Swift!"
+
+//CODE COMMENTS
+
+// ПЕРВЫЙ СПОСОБ. Однострочный комментарий (single-line comment)
+
+// Комментарий в одну строку
+// This is a comment. It is not executed. (Это комментарий. Он не выполняется.)
+
+// Несколько однострочных комментариев сложенных в абзац
+// This is also a comment.
+// Over multiple lines.
+
