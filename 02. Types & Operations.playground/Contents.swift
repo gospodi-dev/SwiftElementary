420110863d0d009748d03fa3903ebdbc9b3ea3a1
Gospodi <gospodijazzet@gmail.com>
2022-01-09 17:15:02 +0300

    Part 1. Type conversion (Преобразование типов)

--- 02. Types & Operations.playground/Contents.swift
+++ 02. Types & Operations.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Part 1. Type conversion (Преобразование типов)
+
+var integer: Int = 100
+var decimal: Double = 12.5
+// Нужно преобразовать выражения к типу Int
+integer = Int(decimal) // 12
+
+// Операторы со смешанными типами (Operators with mixed types)
+let hourlyRate: Double = 19.5
+let hoursWorked: Int = 10
+let totalCost: Double = hourlyRate * Double(hoursWorked)
+
+// Вывод типа (Type inference)
+let typeInferredInt = 42
+let typeInferredDouble = 3.14159
+
+let wantADouble = 3
+//let actuallyDouble = Double(3)
+//let actuallyDouble: Double = 3
+let actuallyDouble = 3 as Double
+
