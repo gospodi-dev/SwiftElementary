1b7391936b63f6e9727388e3230c2e583e5d1804
Gospodi <gospodijazzet@gmail.com>
2022-01-09 20:49:50 +0300

    2.2. Интерполяция (Interpolation)

--- 02. Types & Operations.playground/Contents.swift
+++ 02. Types & Operations.playground/Contents.swift
@@ -40,15 +40,16 @@ let characterDog: Character = "🐶"
 
 let stringDog = "Dog" // Inferred to be of type String (Предполагается, что он имеет тип String)
 
-// Конкатенация (Concatenation)
+// 2.1. Конкатенация (Concatenation)
 
 var message = "Hello" + " my name is "
 let name = "Matt"
 message += name // "Hello my name is Matt"
 
-//let exclamationMark: Character = "!"
-//message += String(exclamationMark) // "Hello my name is Matt!"
+let exclamationMark: Character = "!"
+message += String(exclamationMark) // "Hello my name is Matt!"
 
+/*
 let string1 = "hello"
 let string2 = " there"
 var welcome = string1 + string2 // "hello there"
@@ -77,5 +78,14 @@ print(goodStart + end)
 // one
 // two
 // three
+*/
 
+// 2.2. Интерполяция (Interpolation)
 
+message = "Hello my name is \(name)!" // "Hello my name is Matt!"
+
+let oneThird = 1.0 / 3.0 // 0.3333333333333333
+let oneThirdLongString = "One third is \(oneThird) as a decimal." // "One third is 0.3333333333333333 as a decimal." (Одна треть составляет 0,3333333333333333 в десятичном виде.)
+
+let multiplier = 3
+let messages = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)" // "3 times 2.5 is 7.5"
