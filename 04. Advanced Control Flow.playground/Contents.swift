ee4d3fd483aff22e5f13480d20f9ec4d206ae130
Gospodi <gospodijazzet@gmail.com>
2022-01-14 18:32:19 +0300

    Part 1. Countable ranges (Счетные диапазоны)

--- 04. Advanced Control Flow.playground/Contents.swift
+++ 04. Advanced Control Flow.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Part 1. Countable ranges (Счетные диапазоны)
+// счетный замкнутый диапазон (countable closed range)
+let closedRange = 0...5
+//  Три точки ( ...) указывают на то, что этот диапазон закрыт, что означает, что диапазон изменяется от 0 до 5 включительно. Это цифры (0, 1, 2, 3, 4, 5)
+
+// счетный полуоткрытый диапазон (countable half-open range)
+let halfOpenRange = 0..<5
+
+// A random interlude (Случайная интерлюдия)
+while Int.random(in: 1...6) != 6 {
+    print("Not a six")
+}
