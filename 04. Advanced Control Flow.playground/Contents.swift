61a2e2111d6ffdb7dcc674d1381f01c76944c5c9
Gospodi <gospodijazzet@gmail.com>
2022-01-14 19:22:11 +0300

    Part 2. For loops (Циклы for)

--- 04. Advanced Control Flow.playground/Contents.swift
+++ 04. Advanced Control Flow.playground/Contents.swift
@@ -12,3 +12,26 @@ let halfOpenRange = 0..<5
 while Int.random(in: 1...6) != 6 {
     print("Not a six")
 }
+// Part 2. For loops (Циклы for)
+//for <CONSTANT> in <COUNTABLE RANGE> {
+//  <LOOP CODE>
+//}
+let count = 10
+var sum = 0
+for i in 1...count {
+    sum += 1
+}
+
+sum = 1
+var lastSum = 0
+
+for _ in 0..<count {
+  let temp = sum
+  sum = sum + lastSum
+  lastSum = temp
+}
+
+sum = 0
+for i in 1...count where i % 2 == 1 {
+    sum += i
+}
