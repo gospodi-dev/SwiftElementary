cb79e129755f8fb9f93775e84c7cf495d08f24ce
Gospodi <gospodijazzet@gmail.com>
2022-01-16 18:25:50 +0300

    Part 4. Switch statements (Операторы переключения)

--- 04. Advanced Control Flow.playground/Contents.swift
+++ 04. Advanced Control Flow.playground/Contents.swift
@@ -105,3 +105,125 @@ for row in 0..<8 where row % 2 == 1 {
   }
 }
 
+// Part 4. Switch statements (Операторы переключения)
+let number = 10
+
+switch number {
+case 0:
+    print("Zero")
+default:
+    print("Non-zero")
+}
+// Non-zero
+
+switch number {
+case 10:
+    print("It's ten!")
+default:
+    break
+}
+// It's ten!
+
+let string = "Dog"
+switch string {
+case "Cat", "Dog":
+    print("Animal is a house pet.")
+default:
+    print("Animal is not a house pet.")
+}
+// Animal is a house pet.
+
+// Advanced switch statements (Расширенные операторы переключения)
+
+let hourOfDay = 12
+var timeOfDay = ""
+
+//switch hourOfDay {
+//case 0, 1, 2, 3, 4, 5:
+//  timeOfDay = "Early morning"
+//case 6, 7, 8, 9, 10, 11:
+//  timeOfDay = "Morning"
+//case 12, 13, 14, 15, 16:
+//  timeOfDay = "Afternoon"
+//case 17, 18, 19:
+//  timeOfDay = "Evening"
+//case 20, 21, 22, 23:
+//  timeOfDay = "Late evening"
+//default:
+//  timeOfDay = "INVALID HOUR!"
+//}
+//
+//print(timeOfDay)
+// Afternoon
+
+// можно переписать приведенный выше код, используя диапазоны:
+switch hourOfDay {
+case 0...5:
+  timeOfDay = "Early morning"
+case 6...11:
+  timeOfDay = "Morning"
+case 12...16:
+  timeOfDay = "Afternoon"
+case 17...19:
+  timeOfDay = "Evening"
+case 20..<24:
+  timeOfDay = "Late evening"
+default:
+  timeOfDay = "INVALID HOUR!"
+}
+// Afternoon
+
+switch number {
+case let x where x % 2 == 0:
+    print("Even")
+default:
+    print("Odd")
+}
+// Even
+switch number {
+case _ where number % 2 == 0:
+  print("Even")
+default:
+  print("Odd")
+}
+
+// Partial matching (Частичное совпадение)
+let coordinates = (x: 3, y: 2, z: 5)
+
+switch coordinates {
+case (0, 0, 0): // 1
+  print("Origin")
+case (_, 0, 0): // 2
+  print("On the x-axis.")
+case (0, _, 0): // 3
+  print("On the y-axis.")
+case (0, 0, _): // 4
+  print("On the z-axis.")
+default:        // 5
+  print("Somewhere in space")
+}
+// Somewhere in space
+
+switch coordinates {
+case (0, 0, 0):
+  print("Origin")
+case (let x, 0, 0):
+  print("On the x-axis at x = \(x)")
+case (0, let y, 0):
+  print("On the y-axis at y = \(y)")
+case (0, 0, let z):
+  print("On the z-axis at z = \(z)")
+case let (x, y, z):
+  print("Somewhere in space at x = \(x), y = \(y), z = \(z)")
+}
+
+// Somewhere in space at x = 3, y = 2, z = 5
+
+switch coordinates {
+case let (x, y, _) where y == x:
+  print("Along the y = x line.")
+case let (x, y, _) where y == x * x:
+  print("Along the y = x^2 line.")
+default:
+  break
+}
