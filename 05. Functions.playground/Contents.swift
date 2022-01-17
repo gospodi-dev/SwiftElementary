8f0f424ea27dfd7831d23d0c4cea0a8efec671a9
Gospodi <gospodijazzet@gmail.com>
2022-01-17 13:27:17 +0300

    Part 1. Function basics (Основы работы)

--- 05. Functions.playground/Contents.swift
+++ 05. Functions.playground/Contents.swift
@@ -1,3 +1,11 @@
+import Foundation
 import UIKit
 
-var greeting = "Hello, playground"
+// Part 1. Function basics (Основы работы)
+
+func printMyName() {
+    print("My name is Matt Galloway")
+}
+printMyName()
+// My name is Matt Galloway
+
