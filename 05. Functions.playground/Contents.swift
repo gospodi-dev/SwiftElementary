0f23ca0a4a4ed3de0dfcb8e80c417171b1675bc3
Gospodi <gospodijazzet@gmail.com>
2022-01-17 13:58:20 +0300

    Part 2. Function parameters (Параметры функции)

--- 05. Functions.playground/Contents.swift
+++ 05. Functions.playground/Contents.swift
@@ -9,3 +9,42 @@ func printMyName() {
 printMyName()
 // My name is Matt Galloway
 
+// Part 2. Function parameters (Параметры функции)
+func printMultipleOfFive(value: Int) {
+    print("\(value) * 5 = \(value * 5)")
+}
+printMultipleOfFive(value: 10)
+// 10 * 5 = 50
+
+//func printMultipleOf(multiplier: Int, andValue: Int) {
+//    print("\(multiplier) * \(andValue) = \(multiplier * andValue)")
+//}
+//printMultipleOf(multiplier: 4, andValue: 2)
+//// 4 * 2 = 8
+//
+//func printMultipleOf(multiplier: Int, and value: Int) {
+//  print("\(multiplier) * \(value) = \(multiplier * value)")
+//}
+//printMultipleOf(multiplier: 4, and: 2)
+//// 4 * 2 = 8
+///
+// Если вы хотите вообще не иметь внешнего имени, то можно использовать знак подчеркивания "_"
+func printMultipleOf(_ multiplier: Int, and value: Int) {
+  print("\(multiplier) * \(value) = \(multiplier * value)")
+}
+printMultipleOf(4, and: 2)
+// 4 * 2 = 8
+
+func printMultipleOf(_ multiplier: Int, _ value: Int) {
+  print("\(multiplier) * \(value) = \(multiplier * value)")
+}
+printMultipleOf(4, 2)
+// 4 * 2 = 8
+
+// Вы также можете присвоить параметрам значения по умолчанию
+func printMultipleOf(_ multiplier: Int, _ value: Int = 1) {
+    print("\(multiplier) * \(value) = \(multiplier * value)")
+}
+printMultipleOf(4)
+// 4 * 1 = 4
+
