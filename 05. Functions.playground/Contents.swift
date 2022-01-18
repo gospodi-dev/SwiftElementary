01990b73369e3aeb36eca7567af45ddd5afcd19e
Gospodi <gospodijazzet@gmail.com>
2022-01-18 13:05:49 +0300

    Challenge 3: Recursive functions (Рекурсивные функции)

--- 05. Functions.playground/Contents.swift
+++ 05. Functions.playground/Contents.swift
@@ -211,7 +211,7 @@ for index in stride(from: 10, through: 9, by: -0.1) {
 //9.2
 //9.1
 //9.0
- */
+
 // Challenge 2: It’s prime time (время прайм-тайма)
 /*
  Пишем функцию, чтобы определить, делится ли одно число на другое
@@ -245,3 +245,22 @@ func isPrime(_ number: Int) -> Bool {
 isPrime(6) // false
 isPrime(13) // true
 isPrime(8893) // true
+ */
+// Challenge 3: Recursive functions (Рекурсивные функции)
+func fibonacci(_ number: Int) -> Int {
+  if number <= 0 {
+    return 0
+  }
+
+  if number == 1 || number == 2 {
+    return 1
+  }
+
+  return fibonacci(number - 1) + fibonacci(number - 2)
+}
+fibonacci(1) // 1
+fibonacci(2) // 1
+fibonacci(3) // 2
+fibonacci(4) // 3
+fibonacci(5) // 5
+fibonacci(10) // 55
