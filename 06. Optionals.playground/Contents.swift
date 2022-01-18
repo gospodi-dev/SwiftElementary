c7a39159245a5dc8bc89ec96f0c66921ff66c196
Gospodi <gospodijazzet@gmail.com>
2022-01-18 16:12:57 +0300

    Part 1. Introducing nil and optionals (Представляем nil и опциональные возможности)

--- 06. Optionals.playground/Contents.swift
+++ 06. Optionals.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Part 1. Introducing nil and optionals (Представляем nil и опциональные возможности)
+var name = "Matt Galloway"
+var age = 30
+var occupation = "Software Developer & Author"
+
+var errorCode: Int?
+errorCode = 100
+errorCode = nil
+
+
