765d73d90171feac68da47313ba16501d2e63fee
Gospodi <gospodijazzet@gmail.com>
2022-01-18 16:40:19 +0300

    OPTIONALS INTRODUCTION. Mini-exercises

--- 06. Optionals.playground/Contents.swift
+++ 06. Optionals.playground/Contents.swift
@@ -9,4 +9,11 @@ var errorCode: Int?
 errorCode = 100
 errorCode = nil
 
+// OPTIONALS INTRODUCTION. Mini-exercises
+// 1
+let myFavoriteSong: String? = "Беги за солнцем" // or nil
+// 2
+let parsedInt = Int("10") // возвращает необязательное значение, поскольку строка может не быть целым числом, в этом случае результатом будет nil
+let anotherParsedInt = Int("dog") // nil
 
+// Part 2. Unwrapping optionals (Развертывание опций)
