721b892cd57838c4abe369a177736e9592196b40
Gospodi <gospodijazzet@gmail.com>
2022-01-20 13:13:10 +0300

    Part 1. Arrays (Массивы). Creating arrays (Создание массивов)

--- 07. Arrays, Dictionaries & Sets.playground/Contents.swift
+++ 07. Arrays, Dictionaries & Sets.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Part 1. Arrays (Массивы). Creating arrays (Создание массивов)
+
+//Creating arrays (Создание массивов)
+let evenNumbers = [2, 4, 6, 8]
+// Вы можете создать пустой массив с помощью литерала пустого массива []
+let subscribers: [String] = []
+let allZeros = Array(repeating: 0, count: 5) // [0, 0, 0, 0, 0]
+let vowels = ["A", "E", "I", "O", "U"]
+
