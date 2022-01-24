a43fbbe3c079a8309c45ab6f9f81b2cb5a274358
Gospodi <gospodijazzet@gmail.com>
2022-01-24 16:17:52 +0300

    Shorthand syntax (Сокращенный синтаксис)

--- 08. Collection Iteration With Closures.playground/Contents.swift
+++ 08. Collection Iteration With Closures.playground/Contents.swift
@@ -5,3 +5,44 @@ var multiplyClosure = { (a: Int, b: Int) -> Int in
   return a * b // 8
 }
 let result = multiplyClosure(4, 2) // 8
+/*: ### Shorthand syntax (Сокращенный синтаксис) */
+//: Вариант 1
+multiplyClosure = { (a: Int, b: Int) -> Int in
+  a * b
+}
+//: Вариант 2
+multiplyClosure = { (a, b) in
+  a * b
+}
+//: Вариант 3
+multiplyClosure = {
+  $0 * $1
+}
+func operateOnNumbers(_ a: Int, _ b: Int,
+                      operation: (Int, Int) -> Int) -> Int {
+  let result = operation(a, b)
+  print(result)
+  return result
+}
+let addClosure = { (a: Int, b: Int) in
+  a + b
+}
+operateOnNumbers(4, 2, operation: addClosure)
+// 6
+func addFunction(_ a: Int, _ b: Int) -> Int {
+  a + b
+}
+operateOnNumbers(4, 2, operation: addFunction)
+// 6
+// operateOnNumbers вызывается одинаково, независимо от того, operation является ли это функцией или замыканием.
+operateOnNumbers(4, 2, operation: { (a: Int, b: Int) -> Int in
+  return a + b
+})
+// 6
+
+operateOnNumbers(4, 2, operation: { $0 + $1 })
+operateOnNumbers(4, 2, operation: +) // 6
+operateOnNumbers(4, 2) {
+  $0 + $1
+}
+// 6
