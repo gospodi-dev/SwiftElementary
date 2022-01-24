6fe04abdc68f518788d749aa4dce7a07b2a21866
Gospodi <gospodijazzet@gmail.com>
2022-01-24 16:01:51 +0300

    Closure basics (Основы замыкания)

--- 08. Collection Iteration With Closures.playground/Contents.swift
+++ 08. Collection Iteration With Closures.playground/Contents.swift
@@ -1,3 +1,7 @@
-import UIKit
-
-var greeting = "Hello, playground"
+/*: ## Closures (Замыкания) */
+/*: ### Closure basics (Основы замыкания) */
+// var multiplyClosure: (Int, Int) -> Int
+var multiplyClosure = { (a: Int, b: Int) -> Int in
+  return a * b // 8
+}
+let result = multiplyClosure(4, 2) // 8
