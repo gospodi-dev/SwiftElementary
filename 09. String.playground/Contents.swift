51a06a532b12ef32877a0eaff5a8169af5c0ae81
Gospodi <gospodijazzet@gmail.com>
2022-01-26 18:38:06 +0300

    Part 2. Grapheme clusters (Графемные кластеры)

--- 09. String.playground/Contents.swift
+++ 09. String.playground/Contents.swift
@@ -14,3 +14,21 @@ print(stringLength) // 4
 
 //  let fourthChar = string[3] // error: 'subscript(_:)' is unavailable: cannot subscript String with an Int, use a String.Index instead. (ошибка: 'subscript(_:)' недоступна: невозможно подзаписать String с Int, вместо этого используйте String.Index.)
 
+/*: ### Part2. Grapheme clusters (Графемные кластеры) */
+//: a grapheme cluster
+let cafeNormal = "café"
+let cafeCombining = "cafe\u{0301}"
+cafeNormal.count // 4
+cafeCombining.count // 4
+
+cafeNormal.unicodeScalars.count     // 4
+cafeCombining.unicodeScalars.count  // 5
+
+for codePoint in cafeCombining.unicodeScalars {
+    print(codePoint.value)
+}
+// 99
+// 97
+// 102
+// 101
+// 769
