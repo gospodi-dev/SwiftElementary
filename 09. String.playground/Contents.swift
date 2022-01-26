d32ddb4f078c623b230ebb61adafb2ff2fe36c4a
Gospodi <gospodijazzet@gmail.com>
2022-01-26 18:09:38 +0300

    Part 1. Strings as collections (Строки как коллекции)

--- 09. String.playground/Contents.swift
+++ 09. String.playground/Contents.swift
@@ -1,1 +1,16 @@
 /*: ## 09. String (Строки) */
+/*: ### Part 1. Strings as collections (Строки как коллекции)*/
+let string = "Matt"
+for char in string {
+    print(char)
+}
+//M
+//a
+//t
+//t
+
+let stringLength = string.count
+print(stringLength) // 4
+
+//  let fourthChar = string[3] // error: 'subscript(_:)' is unavailable: cannot subscript String with an Int, use a String.Index instead. (ошибка: 'subscript(_:)' недоступна: невозможно подзаписать String с Int, вместо этого используйте String.Index.)
+
