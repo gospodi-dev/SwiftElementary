58ff5f8008f131740bb597f587b3335953d929ed
Gospodi <gospodijazzet@gmail.com>
2022-01-28 12:30:09 +0300

    Challenge 4: Components (Компоненты)

--- 09. String.playground/Contents.swift
+++ 09. String.playground/Contents.swift
@@ -282,3 +282,26 @@ if let sanitizedName = sanitized(name: "Galloway, Matt") {
 }
 // > Matt Galloway
 
+/*: ### Challenge 4: Components (Компоненты) */
+func splitting(_ string: String, delimiter: Character) -> [String] {
+  var returnArray: [String] = []
+  var lastWordIndex = string.startIndex
+
+  for i in string.indices {
+    if string[i] == delimiter {
+      let substring = string[lastWordIndex..<i]
+      returnArray.append(String(substring))
+      lastWordIndex = string.index(after: i)
+    }
+  }
+
+  // Add the final word
+  let substring = string[lastWordIndex..<string.endIndex]
+  returnArray.append(String(substring))
+
+  return returnArray
+}
+
+let pieces = splitting("Dog,Cat,Badger,Snake,Lion", delimiter: ",")
+print(pieces)
+// > ["Dog", "Cat", "Badger", "Snake", "Lion"]
