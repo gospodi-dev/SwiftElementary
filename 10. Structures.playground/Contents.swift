1c6fa26afc9033d4f5c2226f9fff8664aee7a4b6
Gospodi <gospodijazzet@gmail.com>
2022-02-01 15:07:36 +0300

    Part 01. Introducing structures (Представление структур)

--- 10. Structures.playground/Contents.swift
+++ 10. Structures.playground/Contents.swift
@@ -1,3 +1,48 @@
-import UIKit
+/*: ## 10. Structures (Структуры) */
+/*: ### Part 01. Introducing structures (Представление структур) */
+let restaurantLocation = (3, 3)
+let restaurantRange = 2.5
 
-var greeting = "Hello, playground"
+/*
+// Pythagorean Theorem 📐🎓
+func distance(from source: (x: Int, y: Int),
+              to target: (x: Int, y: Int)) -> Double {
+  let distanceX = Double(source.x - target.x)
+  let distanceY = Double(source.y - target.y)
+  return (distanceX * distanceX +
+    distanceY * distanceY).squareRoot()
+}
+func isInDeliveryRange(location: (x: Int, y: Int)) -> Bool {
+  let deliveryDistance = distance(from: location,
+                                  to: restaurantLocation) // 2.82842712474619
+  return deliveryDistance < restaurantRange
+}
+
+isInDeliveryRange(location: (x: 5, y: 5)) // false
+*/
+
+let otherRestaurantLocation = (8, 8)
+let otherRestaurantRange = 2.5
+
+// Pythagorean Theorem 📐🎓
+func distance(from source: (x: Int, y: Int),
+              to target: (x: Int, y: Int)) -> Double {
+  let distanceX = Double(source.x - target.x)
+  let distanceY = Double(source.y - target.y)
+  return (distanceX * distanceX +
+    distanceY * distanceY).squareRoot()
+}
+
+func isInDeliveryRange(location: (x: Int, y: Int)) -> Bool {
+  let deliveryDistance =
+    distance(from: location, to: restaurantLocation)
+  
+  let secondDeliveryDistance =
+    distance(from: location, to: otherRestaurantLocation) // 4.242640687119285
+  
+  return deliveryDistance < restaurantRange ||
+    secondDeliveryDistance < otherRestaurantRange
+}
+
+isInDeliveryRange(location: (x: 5, y: 5)) // false
+
