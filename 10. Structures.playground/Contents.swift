b3c9b31b556edd7e33674b4c417be1ab75479866
Gospodi <gospodijazzet@gmail.com>
2022-02-01 17:25:48 +0300

    Part 04. Introducing methods (Внедрение методов)

--- 10. Structures.playground/Contents.swift
+++ 10. Structures.playground/Contents.swift
@@ -58,6 +58,14 @@ let storeLocation = Location(x: 3, y: 3)
 struct DeliveryArea {
   let center: Location
   var radius: Double
+    
+  func contains(_ location: Location) -> Bool {
+      let distanceFromCenter =
+        distance(from: (center.x, center.y),
+                   to: (location.x, location.y))
+
+      return distanceFromCenter < radius
+    }
 }
 
 var storeArea = DeliveryArea(center: storeLocation, radius: 2.5)
@@ -83,3 +91,32 @@ storeArea.radius = 3.5
 //fixedArea.radius = 3.5
 
 /*: ### Mini-exercise 2. (Refactoring isInDeliveryRange) */
+
+/*: ### Part 04. Introducing methods (Внедрение методов) */
+let areas = [
+  DeliveryArea(center: Location(x: 3, y: 3), radius: 2.5),
+  DeliveryArea(center: Location(x: 8, y: 8), radius: 2.5)
+]
+
+func isInDeliveryRange(_ location: Location) -> Bool {
+  for area in areas {
+    let distanceToStore =
+      distance(from: (area.center.x, area.center.y),
+                 to: (location.x, location.y))
+
+    if distanceToStore < area.radius {
+      return true
+    }
+  }
+  return false
+}
+
+let customerLocation1 = Location(x: 5, y: 5)
+let customerLocation2 = Location(x: 7, y: 7)
+
+isInDeliveryRange(customerLocation1) // false
+isInDeliveryRange(customerLocation2) // true
+
+let area = DeliveryArea(center: Location(x: 8, y: 8), radius: 2.5)
+let customerLocation = Location(x: 7, y: 7)
+area.contains(customerLocation) // true
