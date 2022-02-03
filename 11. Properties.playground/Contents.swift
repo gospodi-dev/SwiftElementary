dd8ad4ef3d770d6c67acac6d774f652a38963585
Gospodi <gospodijazzet@gmail.com>
2022-02-03 21:40:07 +0300

    Part 01. Stored properties (Хранимые свойства)

--- 11. Properties.playground/Contents.swift
+++ 11. Properties.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+/*: ## 11. Properties (Свойства) */
+/*: ### Part 01. Stored properties (Хранимые свойства) */
 
-var greeting = "Hello, playground"
+struct Contact {
+    var fullName: String
+    var emailAddress: String
+}
+
+var person = Contact(fullName: "Grace Murray",
+                 emailAddress: "grace@navy.mil")
+
+person.fullName // "Grace Murray"
+person.emailAddress // "grace@navy.mil"
+
+person.fullName = "Grace Hopper"
+person.fullName // Grace Hopper
+
+struct Contact {
+  var fullName: String
+  let emailAddress: String
+}
+
+person.emailAddress = "grace@gmail.com"
+// Error: cannot assign to a constant. error: type of expression is ambiguous without more context (Ошибка: невозможно присвоить константу. ошибка: тип выражения неоднозначен без дополнительного контекста)
