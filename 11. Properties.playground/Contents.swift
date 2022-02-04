7920ff5b1e0da1efa53799fe1ea4e770426b9379
Gospodi <gospodijazzet@gmail.com>
2022-02-04 14:09:57 +0300

    Part 03. Computed properties (Вычисляемые свойства)

--- 11. Properties.playground/Contents.swift
+++ 11. Properties.playground/Contents.swift
@@ -30,12 +30,33 @@ struct Contact {
   let emailAddress: String
   var relationship = "Friend"
 }
-var person = Contact(fullName: "Grace Murray",
+var person = Contact(fullName: "Grace Murran",
                      emailAddress: "grace@navy.mil")
 person.relationship // Friend
 
-var boss = Contact(fullName: "Ray Wenderlich",
-                   emailAddress: "ray@raywenderlich.com",
+var boss = Contact(fullName: "Serg Lackrua",
+                   emailAddress: "serg@lackrua.com",
                    relationship: "Boss")
 person.relationship // Friend
 boss.relationship // Boss
+
+/*: ### Part 03. Computed properties (Вычисляемые свойства) */
+struct TV {
+  var height: Double
+  var width: Double
+  
+  // 1
+  var diagonal: Int {
+    // 2
+    let result = (height * height +
+      width * width).squareRoot().rounded() // 110
+    // 3
+    return Int(result) // 110
+  }
+}
+
+var tv = TV(height: 53.93, width: 95.87)
+tv.diagonal // 110
+
+tv.width = tv.height
+tv.diagonal // 76
