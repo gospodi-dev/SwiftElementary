c295161871676ecd5a568f68f42c039b08df9b1c
Gospodi <gospodijazzet@gmail.com>
2022-02-04 16:26:48 +0300

    Properties Challenge 2

--- 11. Properties.playground/Contents.swift
+++ 11. Properties.playground/Contents.swift
@@ -256,3 +256,30 @@ var iceCream = IceCream()
 iceCream.ingredients.append("strawberries")
 // ingredients initialized before appending ["sugar", "milk", "yolks", "cream", "vanilla", "strawberries"]
 iceCream.name = "Strawberry"
+
+/*: ## Properties Challenge 2 */
+struct FuelTank {
+
+  var lowFuel: Bool
+  var level: Double { // decimal percentage between 0 and 1
+    didSet {
+      if level < 0 {
+        level = 0
+      }
+      if level > 1 {
+        level = 1
+      }
+      lowFuel = level < 0.1
+    }
+  }
+}
+
+struct Car {
+  let make: String
+  let color: String
+  var tank: FuelTank
+}
+
+var car = Car(make: "DeLorean", color: "Silver", tank: FuelTank(lowFuel: false, level: 1))
+car.tank.level = -1 // level: 0, lowFuel: true
+car.tank.level = 1.1 // level: 1, lowFuel: false
