51d9af62c0402043249bdcc0110131ebd4c2864b
Gospodi <gospodijazzet@gmail.com>
2022-02-04 13:53:36 +0300

    Part 02. Default values (Значения по умолчанию)

--- 11. Properties.playground/Contents.swift
+++ 11. Properties.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*: ## 11. Properties (Свойства) */
 /*: ### Part 01. Stored properties (Хранимые свойства) */
-
+/*
 struct Contact {
     var fullName: String
     var emailAddress: String
@@ -22,3 +22,20 @@ struct Contact {
 
 person.emailAddress = "grace@gmail.com"
 // Error: cannot assign to a constant. error: type of expression is ambiguous without more context (Ошибка: невозможно присвоить константу. ошибка: тип выражения неоднозначен без дополнительного контекста)
+*/
+
+/*: ### Part 02. Default values (Значения по умолчанию) */
+struct Contact {
+  var fullName: String
+  let emailAddress: String
+  var relationship = "Friend"
+}
+var person = Contact(fullName: "Grace Murray",
+                     emailAddress: "grace@navy.mil")
+person.relationship // Friend
+
+var boss = Contact(fullName: "Ray Wenderlich",
+                   emailAddress: "ray@raywenderlich.com",
+                   relationship: "Boss")
+person.relationship // Friend
+boss.relationship // Boss
