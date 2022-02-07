fc3b75b20747cb02a8a5c76606f993fef4dfcc94
Gospodi <gospodijazzet@gmail.com>
2022-02-07 18:06:42 +0300

    Methods Mini-exercise, Months Until Winter Break

--- 12. Methods.playground/Contents.swift
+++ 12. Methods.playground/Contents.swift
@@ -1,6 +1,6 @@
 /*: # 12. Methods (Методы) */
 /*: ## Part 01. Method refresher (Обновление метода) */
-
+/*
 var numbers = [1, 2, 3]
 numbers.removeLast()
 print(numbers) // [1, 2]
@@ -23,16 +23,55 @@ let months = ["January", "February", "March",
 
 // Создать метод так же просто, как переместить функцию внутрь определения структуры
 
+//struct SimpleDate {
+//  var month: String
+//
+//  func monthsUntilWinterBreak(from date: SimpleDate) -> Int {
+//    months.firstIndex(of: "December")! -
+//    months.firstIndex(of: date.month)!
+//  }
+//}
+
+//let date = SimpleDate(month: "October")
+//date.monthsUntilWinterBreak(from: date) // 2
+
+// date.monthsUntilWinterBreak() // ошибка: отсутствует аргумент для параметра 'from' в вызове
+
+/*: ## Part 02. Introducing self (Представляем self)  */
+
+struct SimpleDate {
+  var month: String
+    // 1
+    func monthsUntilWinterBreak() -> Int {
+      // 2
+//      months.firstIndex(of: "December")! -
+//        months.firstIndex(of: self.month)!
+        // 3
+        months.firstIndex(of: "December")! -
+          months.firstIndex(of: month)!
+    }
+
+}
+
+let date = SimpleDate(month: "October")
+date.monthsUntilWinterBreak() // 2
+
+*/
+
+/*: ### Methods Mini-exercise, Months Until Winter Break */
+
+let months = ["January", "February", "March",
+              "April", "May", "June",
+              "July", "August", "September",
+              "October", "November", "December"]
+
 struct SimpleDate {
   var month: String
   
-  func monthsUntilWinterBreak(from date: SimpleDate) -> Int {
-    months.firstIndex(of: "December")! -
-    months.firstIndex(of: date.month)!
+  var monthsUntilWinterBreak: Int {
+    months.firstIndex(of: "December")! - months.firstIndex(of: month)!
   }
 }
 
 let date = SimpleDate(month: "October")
-date.monthsUntilWinterBreak(from: date) // 2
-
-// date.monthsUntilWinterBreak() // ошибка: отсутствует аргумент для параметра 'from' в вызове
+date.monthsUntilWinterBreak // 2
