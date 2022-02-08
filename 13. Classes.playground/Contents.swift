78b81c67af1f16b3915f579fab2df1c9856ade59
Gospodi <gospodijazzet@gmail.com>
2022-02-08 16:43:06 +0300

    Part 03. Object identity (Идентичность объектов)

--- 13. Classes.playground/Contents.swift
+++ 13. Classes.playground/Contents.swift
@@ -82,3 +82,44 @@ homeOwner.lastName = "Googleseed"
 
 print(john.fullName) // Johnny Googleseed
 print(homeOwner.fullName) // Johnny Googleseed
+
+/*: ## Part 03. Object identity (Идентичность объектов) */
+
+john === homeOwner // true
+
+let imposterJohn = Person(firstName: "Johnny",
+                          lastName: "Appleseed")
+
+john === homeOwner // true
+john === imposterJohn // false
+imposterJohn === homeOwner // false
+
+// Assignment of existing variables changes the instances the variables reference (Присвоение существующих переменных изменяет экземпляры, на которые ссылаются переменные).
+homeOwner = imposterJohn  // Person
+john === homeOwner // false
+
+homeOwner = john // Person
+john === homeOwner // true
+
+
+// Check to ensure the real John is not found among the imposters (Проверьте, не находится ли настоящий Джон среди самозванцев).
+imposters.contains {
+  $0 === john
+} // false
+
+// Now hide the "real" John somewhere among the imposters (Теперь спрячьте "настоящего" Джона где-нибудь среди самозванцев.).
+imposters.insert(john, at: Int.random(in: 0..<100))
+
+// John can now be found among the imposters (Теперь Джона можно найти среди самозванцев.).
+imposters.contains {
+  $0 === john
+} // true
+
+// Since `Person` is a reference type, you can use === to grab the real John out of the list of imposters and modify the value (Поскольку `Person` является ссылочным типом, вы можете использовать ===, чтобы взять настоящего Джона из списка самозванцев и изменить значение).
+// The original `john` variable will print the new last name! (В исходной переменной `john` будет выведена новая фамилия)
+if let indexOfJohn = imposters.firstIndex(where:
+                                          { $0 === john }) {
+  imposters[indexOfJohn].lastName = "Bananapeel"
+}
+
+john.fullName // John Bananapeel
