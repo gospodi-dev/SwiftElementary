bd55829af93c113e4856001ad4a6346a37067fab
Gospodi <gospodijazzet@gmail.com>
2022-02-08 15:54:56 +0300

    Part 01. Creating classes (Создание классов)

--- 13. Classes.playground/Contents.swift
+++ 13. Classes.playground/Contents.swift
@@ -1,2 +1,19 @@
 /*: # Глава 13. Classes (Классы) */
-/*: ## Part 01. */
+/*: ## Part 01. Creating classes (Создание классов) */
+
+class Person {
+  var firstName: String
+  var lastName: String
+
+  init(firstName: String, lastName: String) {
+    self.firstName = firstName
+    self.lastName = lastName
+  }
+
+  var fullName: String {
+    "\(firstName) \(lastName)"
+  }
+}
+
+let john = Person(firstName: "Johnny", lastName: "Appleseed")
+
