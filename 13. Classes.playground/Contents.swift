8ac4d6c49cf159eb0a492875b95a89f4ec848563
Gospodi <gospodijazzet@gmail.com>
2022-02-08 16:27:55 +0300

    Part 02. Reference types (Ссылочные типы)

--- 13. Classes.playground/Contents.swift
+++ 13. Classes.playground/Contents.swift
@@ -17,3 +17,44 @@ class Person {
 
 let john = Person(firstName: "Johnny", lastName: "Appleseed")
 
+/*: ## Part 02. Reference types (Ссылочные типы) */
+
+//class SimplePerson {
+//  let name: String
+//  init(name: String) {
+//    self.name = name
+//  }
+//}
+struct SimplePerson {
+    let name: String
+}
+
+var var1 = SimplePerson(name: "John")
+var var2 = var1
+
+//: Working with references (Работа со ссылками)
+
+struct Location {
+  let x: Int
+  let y: Int
+}
+
+struct DeliveryArea {
+  var range: Double
+  let center: Location
+}
+
+var area1 = DeliveryArea(range: 2.5,
+                         center: Location(x: 2, y: 4))
+var area2 = area1
+print(area1.range) // 2.5
+print(area2.range) // 2.5
+
+area1.range = 4
+print(area1.range) // 4.0
+print(area2.range) // 2.5
+
+var homeOwner = john
+john.firstName = "John" // John wants to use his short name!
+john.firstName // "John"
+homeOwner.firstName // "John"
