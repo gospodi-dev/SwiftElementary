d79b2cacfcba0f61deb1a0381a8f00f703d75f95
Gospodi <gospodijazzet@gmail.com>
2022-02-08 14:24:18 +0300

    Challenge 5: Prime Factors (Прайм-факторы)

--- 12. Methods.playground/Contents.swift
+++ 12. Methods.playground/Contents.swift
@@ -349,3 +349,29 @@ extension Int {
 3.isEven // false
 2.isOdd  // false
 3.isOdd  // true
+
+/*: ### Challenge 5: Prime Factors (Прайм-факторы) */
+extension Int {
+  func primeFactors() -> [Int] {
+    var remainingValue = self
+    var testFactor = 2
+    var primes: [Int] = []
+    while testFactor * testFactor <= remainingValue {
+      if remainingValue % testFactor == 0 {
+        primes.append(testFactor)
+        remainingValue /= testFactor
+      }
+      else {
+        testFactor += 1
+      }
+    }
+    if remainingValue > 1 {
+      primes.append(remainingValue)
+    }
+    return primes
+  }
+}
+
+81.primeFactors() // [3, 3, 3, 3]
+18.primeFactors() // [2, 3, 3]
+57.primeFactors() // [3, 19]
