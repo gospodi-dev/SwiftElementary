b9d91f5804d010df4072317e137afceec80baf39
Gospodi <gospodijazzet@gmail.com>
2022-02-14 17:01:45 +0300

    Part 01. Protocol syntax (Синтаксис протокола)

--- 16. Protocols.playground/Contents.swift
+++ 16. Protocols.playground/Contents.swift
@@ -1,2 +1,22 @@
 /*: # Глава 16. Protocols (Протоколы) */
-/*: ## Part 01.  */
+/*: ## Part 01. Protocol syntax (Синтаксис протокола) */
+
+protocol Vehicle {
+    func accelerate()
+    func stop()
+}
+
+// let vehicle = Vehicle() // error: 'Vehicle' cannot be constructed because it has no accessible initializers (ошибка: 'Vehicle' не может быть построен, потому что у него нет доступных инициализаторов)
+
+class Unicycle: Vehicle {
+    var peddling = false
+    
+    func accelerate() {
+        peddling = true
+    }
+    
+    func stop() {
+        peddling = false
+    }
+}
+
