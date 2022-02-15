392ca5689015883c4b14e6b724112cf593729b36
Gospodi <gospodijazzet@gmail.com>
2022-02-15 17:15:07 +0300

    Part 02. Anatomy of generic types (Анатомия дженерик типов)

--- 17. Generics.playground/Contents.swift
+++ 17. Generics.playground/Contents.swift
@@ -1,5 +1,6 @@
 /*: # Глава 17. Generics (Дженерики) */
 /*: ## Part 01. Introducing generics (Внедрение дженериков) */
+
 enum PetKind {
     case cat
     case dog
@@ -19,3 +20,40 @@ enum EnumKeeperKind {
 }
 
 
+/*: ## Part 02. Anatomy of generic types (Анатомия дженерик типов) */
+
+class Keeper<Animal> {}
+
+// var aCatKeeper = Keeper<Cat>()
+
+class Cat {
+  var name: String
+
+  init(name: String) {
+    self.name = name
+  }
+}
+
+class Dog {
+  var name: String
+
+  init(name: String) {
+    self.name = name
+  }
+}
+
+class Keeper<Animal> {
+  var name: String
+  var morningCare: Animal
+  var afternoonCare: Animal
+
+  init(name: String, morningCare: Animal, afternoonCare: Animal) {
+    self.name = name
+    self.morningCare = morningCare
+    self.afternoonCare = afternoonCare
+  }
+}
+
+let jason = Keeper(name: "Jason",
+                   morningCare: Cat(name: "Whiskers"),
+                   afternoonCare: Cat(name: "Sleepy"))
