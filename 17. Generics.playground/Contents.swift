88cfdc3fc6fe97c188f2ce7b2da07d0ec26d3b81
Gospodi <gospodijazzet@gmail.com>
2022-02-15 16:49:44 +0300

    Part 01. Introducing generics (Внедрение дженериков)

--- 17. Generics.playground/Contents.swift
+++ 17. Generics.playground/Contents.swift
@@ -1,2 +1,21 @@
 /*: # Глава 17. Generics (Дженерики) */
-/*: ## Part 01. */
+/*: ## Part 01. Introducing generics (Внедрение дженериков) */
+enum PetKind {
+    case cat
+    case dog
+//  case bird
+}
+
+struct KeeperKind {
+    var keeperOf: PetKind
+}
+
+let catKeeper = KeeperKind(keeperOf: .cat)
+let dogKeeper = KeeperKind(keeperOf: .dog)
+
+enum EnumKeeperKind {
+  case catKeeper
+  case dogKeeper
+}
+
+
